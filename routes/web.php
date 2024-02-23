14cc3f93571b7924b2fe7841076b96c65ce64dcb
Arya Chandra Kusuma <aryachandrak14@gmail.com>
2024-02-23 10:25:57 +0700

    Route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,41 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//menggunakan callback function
+Route::get('/hello', function() {
+    return 'Hello World';
+});
+
+Route::get('/world', function() {
+    return 'World';
+});
+
+Route::get('/welcome', function(){
+    return "Selamat Datang";
+});
+
+Route:: get('/about', function(){
+    return "Arya Chandra Kusuma 2241720228";
+});
+
+Route::get('/user/{name}', function($name){
+    return "Nama saya ".$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId){
+    return "Post ke-".$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function($id){
+    return "Halaman Artikel dengan ID ".$id;
+});
+
+Route::get('/user/{name?}', function ($name='John'){
+    return "Nama saya ".$name;
+});
+
+Route::get('/user/profile', function(){
+
+})->name('namaprofile');
+
